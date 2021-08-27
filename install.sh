c3f993059c185422cf4c7638b95dceb42825ac33
Zachary Silver <zmansilver@gmail.com>
2021-08-27 15:18:17 -0700

    added pulseaudio to packages and xinit

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ if [ laptop = "TRUE" ]; then
 	sudo pacman -S --noconfirm acpi acpilight
 fi
 
-sudo pacman -S --noconfirm xorg-server xorg-xinit xterm xorg-xrandr xorg-xsetroot xorg-xprop playerctl wget picom ttf-font-awesome alsa-utils xcb-util-xrm scrot feh lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings lxappearance ranger w3m udiskie dunst cmake xbindkeys
+sudo pacman -S --noconfirm xorg-server xorg-xinit xterm xorg-xrandr xorg-xsetroot xorg-xprop playerctl wget picom ttf-font-awesome alsa-utils pulseaudio pulseaudio-alsa pulsemixer xcb-util-xrm scrot feh lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings lxappearance ranger w3m udiskie dunst cmake xbindkeys
 
 sudo pacman -S --noconfirm discord arandr arc-gtk-theme evince gvfs gimp libreoffice htop neofetch openssh firefox vim yarn npm imagemagick rust rustup rust-docs
 
@@ -126,6 +126,7 @@ fi
 cd $HOME/Programs/
 git clone https://github.com/pcaro90/hermit.git && cd hermit/packages/
 gzip -d otf-hermit-2.0.tar.gz && tar xf otf-hermit-2.0.tar
+sudo mkdir /usr/share/fonts/OTF
 sudo cp *.otf /usr/share/fonts/OTF/
 ####################################################################
 
