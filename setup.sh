a7f897521616e89c1126923820045758af7f57d7
Zachary Silver <zmansilver@gmail.com>
2022-02-05 15:06:19 -0800

    added python language server to setup

--- setup.sh
+++ setup.sh
@@ -65,7 +65,7 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 sudo pacman -S --noconfirm neovim clang rustup
-sudo npm install -g typescript-language-server
+sudo npm install -g typescript-language-server pyright
 rustup update && rustup component add rls rust-analysis rust-src
 
 nvim +PlugInstall +qall
